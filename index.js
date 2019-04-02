12d2567aecbfc35f137304edaa09278d19602a0b
Logan Hufstetler <logan.hufstetler@gmail.com>
2019-04-01 19:02:49 -0600

    initialized knex and wrote knexfile. Also, migrated latest users.

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const bcrypt = require('bcryptjs');
+
+const db = require('./database/dbConfig.js');
+const Users = require('.users/user-model.js');
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+server.use(corse());
+
+const port = process.env.PORT || 5000;
+server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
