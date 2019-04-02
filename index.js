014b0bcbcc553271fa4a0f799d853a207084fb7c
Logan Hufstetler <logan.hufstetler@gmail.com>
2019-04-01 20:46:18 -0600

    can successfully check username and password of a user.

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
+// ********** REGISTER ********** //
 server.post('/api/register', async (req, res) => {
   const register = req.body;
 
@@ -25,6 +26,25 @@ server.post('/api/register', async (req, res) => {
     }
   } else {
     res.status(400).json({ message: "You must include a username and password" });
+  }
+});
+
+// ********** LOGIN ********** //
+server.post('/api/login', async (req, res) => {
+  const login = req.body;
+  if(login.username && login.password) {
+    const user = await Users.findBy({ username: login.username }).first();
+    try {
+      if(bcrypt.compareSync(login.password, user.password)) {
+      res.status(200).json({ message: `Hello, ${user.username}! You are now logged in.` });
+      } else {
+      res.status(401).json({ message: "Password is incorrect!" });
+      }
+    } catch (err) {
+      res.status(500).json({ message: "server cannot process this right now" });
+    }  
+  } else {
+    res.status(400).json({ message: "You must put in your username and password if you want to login" });
   }
 });
 
